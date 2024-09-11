0cbba2b1f8da1d05361256903a51f5c7b1f2da54
Benjamin Le <benjaminle2002@gmail.com>
2024-09-10 22:00:06 -0400

    react page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,164 @@
 import "./App.css";
+import { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { responses } from "./responses";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-function App() {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function ModelPerfGraph() {
+  const data = {
+    labels: ["GPT4", "Mixtral"],
+    datasets: [
+      {
+        label: "Accuracy",
+        data: [0.911, 0.673],
+        backgroundColor: "rgba(75, 192, 192, 0.6)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "Average accuracy of models evaluated by Val's equal_intent metric",
+      },
+    },
+  };
+
   return (
     <div>
-      <h2>Medical Benchmarking Results</h2>
-      <p>Your writeup goes here. Don't forget to include plots!</p>
+      <div style={{ width: "600px", margin: "0 auto" }}>
+        <Bar data={data} options={options} />
+      </div>
+    </div>
+  );
+}
+
+const ResponseCard = () => {
+  const [index, setIndex] = useState(0);
+  const [header, setHeader] = useState(responses[0].model);
+  const [responseText, setResponseText] = useState(responses[0].answer);
+  const [isCorrect, setIsCorrect] = useState(responses[0].correct);
+
+  const handleClick = () => {
+    let newIndex = index === 0 ? 1 : 0;
+    setIndex(newIndex);
+    setHeader(responses[newIndex].model);
+    setResponseText(responses[newIndex].answer);
+    setIsCorrect(responses[newIndex].correct);
+  };
+
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "10px",
+        borderRadius: "5px",
+        width: "100%",
+      }}
+    >
+      <h3
+        style={{
+          cursor: "pointer",
+          background: isCorrect ? "rgb(182, 255, 182)" : "rgb(255, 182, 182)",
+          padding: "10px",
+          borderRadius: "5px",
+        }}
+        onClick={handleClick}
+      >
+        {header}
+      </h3>
+      <p
+        style={{
+          maxHeight: "200px",
+          overflow: "scroll",
+        }}
+      >
+        {responseText}
+      </p>
+    </div>
+  );
+};
+function App() {
+  const headers = [
+    "GPT4 and Mixtral's relative performance",
+    "A case study into where GPT4 and Mixtral differs",
+  ];
+
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <h2>Table of Contents</h2>
+        <ul>
+          <li>
+            <a href="#section1">{headers[0]}</a>
+          </li>
+          <li>
+            <a href="#section2">{headers[1]}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="content">
+        <h1>Comparing GPT4 and Mixtral on specialized medical data</h1>
+        <section id="section1">
+          <h2>{headers[0]}</h2>
+          <ModelPerfGraph />
+          <p>
+            As seen by evaluating our models on intent-accuracy, GPT-4
+            significantly outperforms Mixtral 8x7B for some key reasons. First,
+            GPT-4 has nearly 40 times more parameters than Mixtral, making it a
+            much larger and more complex model. This vast number of parameters,
+            combined with its training on an extensive and diverse dataset,
+            allows GPT-4 to capture more intricate patterns and nuances,
+            especially in specialized domains like medicine. In contrast,
+            Mixtral 8x7B, though highly efficient with is Sparse Mixture of
+            Experts (SMOE) architecture, might not have the same depth of
+            inference for such complex tasks due to its smaller size and
+            training scope. As a benefit Mixtral's design allows for faster
+            processing times and lower computational costs, making it highly
+            suitable for general-purpose tasks. However, this efficiency can
+            sometimes come at the cost of the deep inference required for
+            specialized applications, like medical recognition.
+          </p>
+        </section>
+        <section id="section2">
+          <h2>{headers[1]}</h2>
+          <p style={{ fontWeight: "bold" }}>
+            Q: How to diagnose Parasites - Trichuriasis (also known as Whipworm
+            Infection) ?
+          </p>
+          <ResponseCard />
+          <p>
+            It appears that GPT-4 outperforms Mistral in synthesizing data and
+            providing direct answers to questions. Mistral tends to simply
+            repeat information, without ensuring that the responses are clear
+            and concise. This highlights GPT-4&apos;s superior ability to draw
+            inferences from specialized datasets and demonstrates a deeper
+            understanding of the information provided.
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/responses.tsx
@@ -0,0 +1,98 @@
+const responses = [
+  {
+    model: "GPT4",
+    answer: `Diagnosing Trichuriasis, caused by the whipworm Trichuris trichiura, involves a combination of clinical evaluation and laboratory tests. The diagnosis is primarily based on the detection of eggs or adult worms in stool samples, as the symptoms can be nonspecific and similar to other gastrointestinal disorders. Here's a step-by-step approach to diagnosing Trichuriasis:
+
+    1. Clinical Evaluation
+    
+        Symptom Assessment: The clinician will assess symptoms which may include abdominal pain, diarrhea, rectal prolapse (in severe cases), and other signs of malnutrition or anemia in chronic infections.
+        Medical History: A detailed medical history that includes questions about the patient's travel history, especially to areas where Trichuriasis is endemic, and exposure to unsanitary conditions can provide clues.
+    
+    2. Laboratory Tests
+    
+        Stool Microscopy: The most common and definitive method for diagnosing Trichuriasis is the microscopic examination of stool samples for the presence of Trichuris trichiura eggs. Because egg production might be irregular, it is recommended to examine samples from at least three different days.
+            Direct Smear: A small sample of stool is mixed with saline or iodine and examined under a microscope. This method is quick but less sensitive.
+            Concentration Techniques: These methods, such as the formalin-ethyl acetate sedimentation technique, increase the sensitivity of egg detection by concentrating the eggs from a larger volume of stool.
+        Kato-Katz Technique: This quantitative method involves smearing a known quantity of stool on a slide that is then cleared with cellophane soaked in malachite green or another clearing solution. It's particularly useful in epidemiological studies to estimate the worm burden.
+    
+    3. Other Diagnostic Methods
+    
+        Molecular Diagnostics: PCR (Polymerase Chain Reaction) and other molecular techniques can detect Trichuris trichiura DNA in stool samples. These methods are more sensitive and specific but are usually available only in specialized laboratories or for research purposes.
+        Colonoscopy: In rare cases, adult worms can be visualized in the colon or rectum during a colonoscopy, but this method is not routinely used for diagnosis.
+    
+    4. Differential Diagnosis
+    
+    Given that the symptoms of Trichuriasis can be similar to other gastrointestinal infections, it's important to rule out other causes of the symptoms, such as bacterial or
+
+`,
+    correct: true,
+  },
+  {
+    model: "Mixtral",
+    answer: `Trichuriasis (also known as Whipworm Infection) is a parasitic infection caused by the nematode Trichuris trichiura.
+
+  Symptoms
+  
+  Most people infected with whipworms have no symptoms.
+  
+  If symptoms do occur, they may include:
+  
+      Abdominal pain
+      Diarrhea
+      Fatigue
+      Weight loss
+  
+  Causes
+  
+  Whipworm infection is caused by the parasite Trichuris trichiura.
+  
+  The eggs of the parasite are passed in the stool. The eggs can survive for up to 5 weeks in the soil.
+  
+  People become infected when they swallow the eggs. This can happen when hands or fingers that have contaminated dirt on them are put in the mouth. Eggs can also be swallowed by eating raw vegetables or fruits that were grown in contaminated soil.
+  
+  Risk Factors
+  
+  Risk factors for whipworm infection include:
+  
+      Age: children are more likely to be infected
+      Warm, humid climate
+      Poor hygiene
+      Poor sanitation
+      Temperate climates
+  
+  Tests
+  
+  A stool sample is examined under a microscope to look for the presence of whipworm eggs.
+  
+  Treatment
+  
+  The goal of treatment is to get rid of the infection.
+  
+  Medicines called antihelmintics are used to kill the parasites.
+  
+  Expectations (prognosis)
+  
+  The outcome is expected to be good with treatment.
+  
+  Complications
+  
+      Anemia
+      Intestinal obstruction
+      Rectal prolapse
+  
+  Calling your health care provider
+  
+  Call for an appointment with your health care provider if you have symptoms of whipworm infection.
+  
+  Prevention
+  
+  Improved sanitation and hygiene are important in preventing the spread of whipworm infection.
+  
+  People who live in or travel to areas where whipworm infection is common should:
+  
+      Avoid eating raw vegetables or fruits that may have been grown in contaminated soil`,
+    correct: false,
+  },
+];
+
+export { responses };
